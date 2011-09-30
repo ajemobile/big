48eddd13cccc04bae8652596f9dfc9ed40bc6378
Enrique Madrid <emadridm@gmail.com>
2011-09-30 17:06:31 -0500

    backup

--- features/step_definitions/configuration_steps.rb
+++ features/step_definitions/configuration_steps.rb
@@ -1,4 +1,19 @@
+require 'yaml'
+
+Then /^the output should indicate the full name for the YAML file used \(default is "([^"]*)"\)$/ do |file|
+  @filename = File.expand_path(file)
+  Then("the output should contain \"#{@filename}\"")
+end
+
+Then /^the member "([^"]*)" with value "([^"]*)" should be saved into the file$/ do |key, value|
+  config = YAML.load_file(@filename)
+  config.send(key).should == value
+end
+
+
 Then /^the output should contain key "([^"]*)" and value "([^"]*)"$/ do |key, value|
   Then("the output should contain \"#{key}\"")
   Then("the output should contain \"#{value}\"")
 end
+
+
--- lib/bigmagic/config_command.rb
+++ lib/bigmagic/config_command.rb
@@ -11,17 +11,8 @@ module Bigmagic
 
     parameter "[PAIR] ...", "key value pairs separated by space", :attribute_name => :pairs
 
-
-    def load_file(filename = config_filename)
-      set_default
-    end
-
-    def get(string)
-      key = string.split('.').inject(config) {|key,member| key.send(member)}
-    end
-
     def execute
-      out.puts "Configuration file: #{@config_filename}"
+      out.puts "Configuration file: #{config_filename}"
     end
 
   end
